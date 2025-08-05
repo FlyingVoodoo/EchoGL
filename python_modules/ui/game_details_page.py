d311d39fe6a1b53ffb17cdd2cb3774e2ed3daa29
Матвей <motysha1905@gmail.com>
2025-08-05 23:33:48 +0300

    Game Details Page

--- python_modules/ui/game_details_page.py
+++ python_modules/ui/game_details_page.py
@@ -1,5 +1,7 @@
 from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel, QPushButton
 from PyQt6.QtCore import Qt, pyqtSignal, QObject
+from pathlib import Path
+from PyQt6.QtGui import QPixmap
 
 class GameDetailsPage(QWidget):
     launch_game_requested = pyqtSignal(str) 
@@ -28,6 +30,7 @@ class GameDetailsPage(QWidget):
         
         self.detail_info_label = QLabel("Additional info here (ML, stats, description...)")
         self.detail_info_label.setObjectName("detailText")
+        self.detail_info_label.setTextFormat(Qt.TextFormat.RichText)
         self.detail_info_label.setWordWrap(True)
 
         self.detail_content_layout.addWidget(self.detail_cover_label)
@@ -42,14 +45,28 @@ class GameDetailsPage(QWidget):
         self._current_appid = None 
 
     def set_game_info(self, game_info: dict):
+
         self._current_appid = game_info.get('appid')
         
         self.detail_game_name_label.setText(game_info.get('name', 'N/A'))
-        self.detail_info_label.setText(f"AppID: {game_info.get('appid', 'N/A')}\n"
-                                         f"Install Path: {game_info.get('install_path', 'N/A')}\n"
-                                         f"Some future ML data or description goes here...")
+
+        self.detail_info_label.setText(f"AppID: {game_info.get('appid', 'N/A')}<br>"
+                                         f"Install Path: {game_info.get('install_path', 'N/A')}<br><br>"
+                                         f"<b>Genres</b> {game_info.get('genres', 'N/A')}<br>"
+                                         f"<b>Platforms</b> {game_info.get('platforms', 'N/A')}<br><br>"
+                                         f"<b>Summary</b> {game_info.get('summary', 'N/A')}")
         
         self.detail_launch_button.setEnabled(bool(self._current_appid)) 
+
+        cover_path = game_info.get('cover_path')
+        if cover_path and Path(cover_path).exists():
+            pixmap = QPixmap(cover_path)
+            if not pixmap.isNull():
+                self.detail_cover_label.setPixmap(pixmap)
+            else:
+                self.detail_cover_label.clear()
+        else:
+            self.detail_cover_label.clear()
         
         self.game_manager.request_display_cover.emit(
             str(game_info.get('appid')), self.detail_cover_label, 'detail', True
